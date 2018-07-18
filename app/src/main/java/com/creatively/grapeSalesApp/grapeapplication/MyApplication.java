62ccfc2b2d733939669f9737ad625048d397e066
Fatima <fa_salman@hotmail.com>
2018-07-18 19:11:36 +0300

    data

--- app/src/main/java/com/creatively/grapeSalesApp/grapeapplication/MyApplication.java
+++ app/src/main/java/com/creatively/grapeSalesApp/grapeapplication/MyApplication.java
@@ -15,6 +15,7 @@ public class MyApplication extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+        //data
     }
 
 }
